4a77f511635e0e647b74a13e81f7ff6d1d3c9da8
IbrahimAlatoom <ibrahim.alatoom@gmail.com>
2021-03-18 19:06:57 +0200

    item and adapter added

---
+++ app/src/main/java/com/lemonlab/kchallengeapp/fragments/list/QuestionAdapter.kt
@@ -0,0 +1,37 @@
+package com.lemonlab.kchallengeapp.fragments.list
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.lemonlab.kchallengeapp.R
+import com.lemonlab.kchallengeapp.data.Question
+import kotlinx.android.synthetic.main.question_item.view.*
+
+class QuestionAdapter : RecyclerView.Adapter<QuestionAdapter.ViewHolder>() {
+
+    private var questionList = emptyList<Question>()
+
+    class ViewHolder(itemView : View):RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context)
+            .inflate(R.layout.question_item,parent,false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentItem = questionList[position]
+        holder.itemView.id_text.text = currentItem.id.toString()
+        holder.itemView.question_content.text = currentItem.questionText
+    }
+
+    override fun getItemCount(): Int {
+        return questionList.size
+    }
+    fun setData(question: List<Question>){
+        this.questionList = question
+        notifyDataSetChanged()
+    }
+}
