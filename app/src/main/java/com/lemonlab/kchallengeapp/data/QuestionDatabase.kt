e5e85aefad5bb2ce0ab26ad1173cf6b5cd335e88
IbrahimAlatoom <ibrahim.alatoom@gmail.com>
2021-03-18 15:01:49 +0200

    add QuestionDatabase

---
+++ app/src/main/java/com/lemonlab/kchallengeapp/data/QuestionDatabase.kt
@@ -0,0 +1,32 @@
+package com.lemonlab.kchallengeapp.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Question::class],version = 1,exportSchema = false)
+abstract class QuestionDatabase:RoomDatabase() {
+
+    abstract fun questionDao():QuestionDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE : QuestionDatabase ?= null
+        fun getDatabase(context: Context):QuestionDatabase{
+            val tempInstance = INSTANCE
+            if (tempInstance!=null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    QuestionDatabase::class.java,
+                    "question_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
