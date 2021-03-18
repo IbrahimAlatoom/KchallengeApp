53705e9e54e8037fc634b7ea91de8580d2a5d59d
IbrahimAlatoom <ibrahim.alatoom@gmail.com>
2021-03-18 14:52:01 +0200

    add question Dao

---
+++ app/src/main/java/com/lemonlab/kchallengeapp/data/QuestionDao.kt
@@ -0,0 +1,17 @@
+package com.lemonlab.kchallengeapp.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface QuestionDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addQuestion(question: Question)
+
+    @Query("SELECT * FROM questions ORDER BY id ASC")
+    fun readAllData():LiveData<List<Question>>
+}
