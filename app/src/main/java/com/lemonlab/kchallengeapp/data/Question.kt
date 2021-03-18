5fdda13abe794465568eb91e36f1de7e3de87b86
IbrahimAlatoom <ibrahim.alatoom@gmail.com>
2021-03-18 14:26:46 +0200

    add question table

--- app/src/main/java/com/lemonlab/kchallengeapp/data/Question.kt
+++ app/src/main/java/com/lemonlab/kchallengeapp/data/Question.kt
@@ -1,4 +1,18 @@
 package com.lemonlab.kchallengeapp.data
 
-class Question {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "questions")
+data class Question(
+
+    @PrimaryKey(autoGenerate = true)
+    val id : Int ,
+    val category : String ,
+    val questionText : String,
+    val answerOne : String,
+    val answerTwo : String,
+    val answerThree : String,
+    val answerFour : String,
+    val answerIndex: Int
+)
