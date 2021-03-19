c1afce8932ec57a49fdc2767578d8b20f2100372
IbrahimAlatoom <ibrahim.alatoom@gmail.com>
2021-03-19 15:43:14 +0200

    GJSON

--- app/src/main/java/com/lemonlab/kchallengeapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/lemonlab/kchallengeapp/fragments/list/ListFragment.kt
@@ -1,18 +1,25 @@
 package com.lemonlab.kchallengeapp.fragments.list
 
 import android.os.Bundle
+import android.os.Environment
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.Gson
 
 import com.lemonlab.kchallengeapp.R
 import com.lemonlab.kchallengeapp.viewmodel.QuestionViewModel
 
 import kotlinx.android.synthetic.main.fragment_list.view.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.FileWriter
+import java.io.OutputStreamWriter
 
 
 class ListFragment : Fragment() {
@@ -21,7 +28,8 @@ class ListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_list, container, false)
 
@@ -40,6 +48,8 @@ class ListFragment : Fragment() {
         }
 
 
+
+
         view.floatingActionButton.setOnClickListener {
             navigateToAdd()
         }
@@ -51,5 +61,16 @@ class ListFragment : Fragment() {
         findNavController().navigate(R.id.action_listFragment_to_addFragment)
     }
 
+    private fun ToJson() {
+        val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+        val myFile = File(path,"data.json")
+        val fileOut = FileOutputStream(myFile,true)
+        val writer = OutputStreamWriter(fileOut)
+        val gson = Gson()
+        questionViewModel.readAllData.observe(viewLifecycleOwner){
+            var json = gson.toJson(it,writer)
+        }
+
+    }
 
 }
